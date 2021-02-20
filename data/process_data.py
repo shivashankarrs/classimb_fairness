e47c703df041c45d99050b60e377bec68bf0b26a
Shivashankar Subramanian <shivashankarrs@gmail.com>
2021-02-20 18:53:33 +1100

    Sample, bert

--- data/process_data.py
+++ data/process_data.py
@@ -1,25 +1,71 @@
 import pickle
 import numpy as np
-from sklearn.utils import shuffle
+import os, math
+
 
 DATA_PATH = "/Users/sssub/Documents/GitHub/classimb_fairness/class_imbalance/datasets/"
 
-def load_data_deepmoji(path):
-    fnames = ["neg_neg.npy", "neg_pos.npy", "pos_neg.npy", "pos_pos.npy"]
-    protected_labels = [0, 1, 0, 1]
-    main_labels = [0, 0, 1, 1]
-    dataset = []
-    for fname, p_label, m_label in zip(fnames, protected_labels, main_labels):
+def get_sizes(base_size):
+    black = 0.18 #8780
+    white = 0.82 #40000
+    pos = int(base_size + (base_size*0.18/0.82))
+    neg = int(pos * 0.31/0.69)
+    print(base_size, pos, neg)
+    pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size = [math.ceil(pos*black), math.ceil(pos*white), math.ceil(neg*black), \
+                                                          math.ceil(neg*white)]
+    print(pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size)
+    sample_size = [pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size]
+    return sample_size
+
+def get_inlp_sizes(base_size, black_ratio, white_ratio):
+    black = black_ratio
+    white = white_ratio
+    pos = base_size
+    neg = base_size
+    pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size = [math.ceil(pos*black), math.ceil(pos*white), math.ceil(neg*white), \
+                                                          math.ceil(neg*black)]
+    print(pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size)
+    sample_size = [pos_pos_size, pos_neg_size, neg_pos_size, neg_neg_size]
+    return sample_size
+
+def downsample(vecs, size):
+    np.random.seed(1)
+    np.random.shuffle(vecs)
+    return vecs[:size]
+
+def load_data_deepmoji(path, option='original'):
+    fnames = ["pos_pos.npy", "pos_neg.npy", "neg_pos.npy", "neg_neg.npy"]
+    protected_labels = [1, 0, 1, 0]
+    main_labels = [1, 1, 0, 0]
+    xdata = []
+    labels = []
+    protected_attribute = []
+
+    check_data = np.load(path + '/' + "neg_neg.npy")
+    
+    if option=='original':
+        sample_size = get_sizes(check_data.shape[0])
+    elif option=='inlp0.5':
+        sample_size = get_inlp_sizes(check_data.shape[0], 0.5, 0.5)
+    elif option=='inlp0.6':
+        sample_size = get_inlp_sizes(check_data.shape[0], 0.6, 0.4)
+    elif option=='inlp0.7':
+        sample_size = get_inlp_sizes(check_data.shape[0], 0.7, 0.3)
+    elif option=='inlp0.8':
+        sample_size = get_inlp_sizes(check_data.shape[0], 0.8, 0.2)
+        
+    print(check_data.shape[0], sample_size)
+
+    for fname, p_label, m_label, size in zip(fnames, protected_labels, main_labels, sample_size):
         print(path + '/' + fname)
         data = np.load(path + '/' + fname)
-        for x in data:
-            _data = {}
-            _data['features'] = x 
-            _data['protected_feature'] = p_label
-            _data['label'] = m_label
-            dataset.append(_data)
-        print(fname, len(dataset))
-    #dataset = shuffle(dataset, random_state=0)
+        sampled_data = downsample(data, size)
+        for x in sampled_data:
+            xdata.append(x)
+            protected_attribute.append(p_label)
+            labels.append(m_label)
+        print(fname, sampled_data.shape, len(xdata), len(protected_attribute), len(labels))
+    dataset = {'feature': np.array(xdata), 'labels': np.array(labels), 'protected_attribute': np.array(protected_attribute)}
     return dataset
 
 def load_dictionary(path):
@@ -32,18 +78,20 @@ def load_dictionary(path):
         text2index[k] = v
     return text2index
 
-def load_data_biasbios(path, rpath):
-    fulldata = load_dataset(path)
+def load_data_biasbios(path, rpath, xpath):
+    fdata = load_dataset(path)
     prof2index = load_dictionary(rpath)
-    trimdataset = []
-    for data in fulldata:
-        _data = {}
-        _data['text'] = data['hard_text']
-        _data['protected_feature'] = 1 if data['g'] == 'f' else 0
-        _data['label'] = prof2index[data['p']]
-        trimdataset.append(_data)
-    #trimdataset = shuffle(trimdataset, random_state=0)
-    return trimdataset
+    xdata = np.load(xpath)
+    labels = []
+    protected_attribute = []
+    for data in fdata:
+        _protected_feature = 1 if data['g'] == 'f' else 0
+        _label = prof2index[data['p']]
+        labels.append(_label)
+        protected_attribute.append(_protected_feature)
+    print(xdata.shape, len(protected_attribute), len(labels))
+    dataset = {'feature': np.array(xdata), 'labels': np.array(labels), 'protected_attribute': np.array(protected_attribute)}
+    return dataset
 
 def load_dataset(path):
     with open(path, "rb") as f:
@@ -56,5 +104,10 @@ def dump_dataset(path, data):
     return 
 
 if __name__ == "__main__":
-    train_biasbios = load_data_biasbios(DATA_PATH+"biography/train.pickle", "../resources/professions.txt")
-    print (len(train_biasbios), train_biasbios[0])
+    train_biasbios = load_data_biasbios(DATA_PATH+"biography/train.pickle", "../resources/professions.txt", DATA_PATH+"biography/features/train_cls.npy")
+    print (len(train_biasbios), train_biasbios['feature'].shape, train_biasbios['feature'][0].shape)
+
+    #train_biasbios = load_data_deepmoji(DATA_PATH+"deepmoji/test/", option='inlp0.8')
+    #from collections import Counter
+    #print (len(train_biasbios), len(train_biasbios['labels']), train_biasbios['feature'].shape)
+    #print (Counter(train_biasbios['labels']), Counter(train_biasbios['protected_attribute']))
